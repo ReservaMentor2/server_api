9427f894a5bd5b528f9c5ad8c3aa45a8e244aa6d
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-09-03 22:51:20 -0500

    Datos iniciales

---
+++ reservamentor-api/src/test/java/com/reservamentor/ReservamentorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservamentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservamentorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
