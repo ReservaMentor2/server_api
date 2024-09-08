f4c155ec892ad017543654901752fdafe876f66b
Mathias <mathiashv2014@gmail.com>
2024-09-07 21:20:39 -0500

    class estudiante

--- reservamentor-api/src/main/java/com/reservamentor/model/entity/Estudiante.java
+++ reservamentor-api/src/main/java/com/reservamentor/model/entity/Estudiante.java
@@ -1,4 +1,17 @@
 package com.reservamentor.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Estudiante")
 public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "usuarioID", referencedColumnName = "id", foreignKey = @ForeignKey(name = "usuarioID"))
+    private Usuario usuario;
 }
--- reservamentor-api/src/main/java/com/reservamentor/model/entity/Usuario.java
+++ reservamentor-api/src/main/java/com/reservamentor/model/entity/Usuario.java
@@ -4,16 +4,18 @@ import jakarta.persistence.*;
 import lombok.Data;
 @Entity
 @Data
+@Table(name = "Usuario")
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Column(name = "nombre", nullable = false, length = 100)
     private String nombre;
     @Column(name = "apellido", nullable = false, length = 100)
     private String apellido;
     @Column(name = "correo", nullable = false, length = 100)
     private String correo;
-    @Column(name = "telefono", nullable = false, length = 9)
+    @Column(name = "telefono", nullable = true, length = 9)
     private String telefono;
 }
