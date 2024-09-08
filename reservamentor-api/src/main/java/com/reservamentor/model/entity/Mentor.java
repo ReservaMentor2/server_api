4b4a92775849b98e4c76ec0a0525a271cd50730e
Mathias <mathiashv2014@gmail.com>
2024-09-07 21:37:28 -0500

    Class mentor

--- reservamentor-api/src/main/java/com/reservamentor/model/entity/Mentor.java
+++ reservamentor-api/src/main/java/com/reservamentor/model/entity/Mentor.java
@@ -1,4 +1,20 @@
 package com.reservamentor.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+
+@Data
+@Entity
+@Table(name = "Mentor")
 public class Mentor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "valoracionPromedio", nullable = false)
+    private Float valoracionPromedio;
+
+    @OneToOne
+    @JoinColumn(name = "usuarioID", referencedColumnName = "id", foreignKey = @ForeignKey(name = "usuarioID"))
+    private Usuario usuario;
 }
