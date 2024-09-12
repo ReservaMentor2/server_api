90f6246ad9dd6110f29a7f0f3e018b4861809aad
Robert Alvarado <100499519+Treborrr@users.noreply.github.com>
2024-09-12 19:11:43 +0000

    EventoRepository actualizado

---
+++ reservamentor-api/src/main/java/com/reservamentor/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.reservamentor.repository;
+
+import com.reservamentor.model.entity.Evento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventoRepository extends JpaRepository<Evento, Integer> {
+}
