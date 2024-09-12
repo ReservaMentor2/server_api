001bbd367ebfddc2077f7b1a6f9bd0e013d91a02
Mathias <mathiashv2014@gmail.com>
2024-09-12 10:41:46 -0500

    Implemetancion repositorio y servicios

---
+++ reservamentor-api/src/main/java/com/reservamentor/repository/MentoriaRepository.java
@@ -0,0 +1,8 @@
+package com.reservamentor.repository;
+
+import com.reservamentor.model.entity.Sesionmentoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MentoriaRepository extends JpaRepository<Sesionmentoria, Integer>{
+
+}
---
+++ reservamentor-api/src/main/java/com/reservamentor/service/AdminMentoriaService.java
@@ -0,0 +1,13 @@
+package com.reservamentor.service;
+
+import com.reservamentor.model.entity.Sesionmentoria;
+
+import java.util.List;
+
+public interface AdminMentoriaService {
+    Sesionmentoria create(Sesionmentoria sesionmentoria);
+    Sesionmentoria update(Integer id, Sesionmentoria updateSesionMentoria);
+    Sesionmentoria delete (Integer id,Sesionmentoria sesionmentoria);
+    Sesionmentoria findById(Integer id);
+    Sesionmentoria reprogramarMentoria(Integer id, Sesionmentoria updateSesionMentoria);
+}
