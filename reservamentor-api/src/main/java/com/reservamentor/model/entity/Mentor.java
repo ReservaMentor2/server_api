5315df54791a9a9e2006704b63fcf3b32b17273b
Drewks <u202211499@upc.edu.pe>
2024-09-12 22:59:26 -0500

    mentor entity

--- reservamentor-api/src/main/java/com/reservamentor/model/entity/Mentor.java
+++ reservamentor-api/src/main/java/com/reservamentor/model/entity/Mentor.java
@@ -18,26 +18,23 @@ public class Mentor {
     @Id
     @Column(name = "mentorid", nullable = false)
     private Integer id;
-
     @NotNull
     @ManyToOne(fetch = FetchType.EAGER, optional = false) // Cambia a EAGER para cargar usuario automáticamente
     @JoinColumn(name = "usuarioid", nullable = false)
     @JsonIgnore
     private Usuario usuario; // Renombrado de usuarioid a usuario
 
+
     @NotNull
     @Column(name = "valoracionpromedio", nullable = false, precision = 4, scale = 2)
     private BigDecimal valoracionpromedio;
-
     @NotNull
     @Column(name = "tarifahora", nullable = false)
     private Integer tarifahora;
-
     @Size(max = 500)
     @NotNull
     @Column(name = "biografia", nullable = false, length = 500)
     private String biografia;
-
     @OneToMany(mappedBy = "mentor", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     private List<Disponibilidad> horarioDisponible;
