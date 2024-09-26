7d700c72944ad873f6602e31aeaa2feba98b1704
Robert Alvarado <la4132135@gmail.com>
2024-09-26 10:14:47 -0500

    Creacion de RecomendacionMentorController.java. Expone el endpoint que recibe las preferencias del estudiante y devuelve la lista de mentores recomendados.

---
+++ reservamentor-api/src/main/java/com/reservamentor/api/RecomendacionMentorController.java
@@ -0,0 +1,22 @@
+package com.reservamentor.api;
+
+import com.reservamentor.dto.PreferenciasMentorDTO;
+import com.reservamentor.model.entity.Mentor;
+import com.reservamentor.service.RecomendacionMentorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/mentores")
+public class RecomendacionMentorController {
+
+    @Autowired
+    private RecomendacionMentorService recomendacionMentorService;
+
+    @PostMapping("/recomendaciones")
+    public List<Mentor> recomendarMentores(@RequestBody PreferenciasMentorDTO preferencias) {
+        return recomendacionMentorService.recomendarMentores(preferencias);
+    }
+}
