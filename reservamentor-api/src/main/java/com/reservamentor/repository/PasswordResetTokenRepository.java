06c691afd482834f5d702de1536c6a27e6b013f0
JoeTurpo <joeturpo@gmail.com>
2024-10-24 02:48:37 -0500

    Metodos de enviado de correo y autenticación(mediante token)

---
+++ reservamentor-api/src/main/java/com/reservamentor/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,12 @@
+package com.reservamentor.repository;
+
+import com.reservamentor.model.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Integer> {
+    Optional<PasswordResetToken> findByToken(String token);
+
+
+}
