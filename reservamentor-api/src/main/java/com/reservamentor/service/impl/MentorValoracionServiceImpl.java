2a55c0ba5145b42acd1a0ec4e0c6bbbc8464282b
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-11-14 15:13:19 -0500

    Added comments

--- reservamentor-api/src/main/java/com/reservamentor/service/impl/MentorValoracionServiceImpl.java
+++ reservamentor-api/src/main/java/com/reservamentor/service/impl/MentorValoracionServiceImpl.java
@@ -1,9 +1,14 @@
 package com.reservamentor.service.impl;
 
+import com.reservamentor.dto.ValoracionDTO;
+import com.reservamentor.exception.BadRequestException;
 import com.reservamentor.exception.MentorNotFound;
 import com.reservamentor.exception.ResourceNotFoundException;
+import com.reservamentor.mapper.ValoracionMapper;
+import com.reservamentor.model.entity.Estudiante;
 import com.reservamentor.model.entity.Mentor;
 import com.reservamentor.model.entity.Valoracion;
+import com.reservamentor.repository.EstudianteRepository;
 import com.reservamentor.repository.MentorRepository;
 import com.reservamentor.repository.MentorValoracionRepository;
 import com.reservamentor.service.MentorValoracionService;
@@ -18,6 +23,8 @@ public class MentorValoracionServiceImpl implements MentorValoracionService {
 
     private final MentorRepository mentorRepository;
     private final MentorValoracionRepository mentorValoracionRepository;
+    private final EstudianteRepository estudianteRepository;
+    private final ValoracionMapper valoracionMapper;
 
     @Override
     public List<Valoracion> findValoracionesByMentor(Integer mentorId) {
@@ -39,4 +46,36 @@ public class MentorValoracionServiceImpl implements MentorValoracionService {
         );
         return mentorValoracionRepository.calculateAverageRating(mentor);
     }
+
+    @Override
+    public Valoracion createValoracionMentor(Integer estudianteId, Integer mentorId, ValoracionDTO valoracionDTO) {
+        Mentor mentor = mentorRepository.findById(mentorId).orElseThrow(
+                () -> new MentorNotFound("El mentor con ID " + mentorId + " no fue encontrado")
+        );
+
+        Estudiante estudiante = estudianteRepository.findById(estudianteId).orElseThrow(
+                () -> new MentorNotFound("El mentor con ID " + estudianteId + " no fue encontrado")
+        );
+
+
+        if (mentorValoracionRepository.existsByMentoridAndEstudianteid(mentor, estudiante)) {
+            throw new BadRequestException("Ya existe un comentario con el mentor y estudiante");
+        }
+
+        Valoracion valoracion = valoracionMapper.toEntity(valoracionDTO);
+
+        System.out.println(valoracion.getEstrellas());
+        System.out.println(valoracion.getComentario());
+
+        valoracion.setEstudianteid(estudiante);
+        valoracion.setMentorid(mentor);
+
+        System.out.println(valoracion);
+
+        valoracion = mentorValoracionRepository.save(valoracion);
+
+        return valoracion;
+    }
+
+
 }
